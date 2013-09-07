f8e20697f1b4ba8becad938dbb4d4c4fa2d4ad32
Buz Carter <buzcarter@hotmail.com>
2013-09-07 09:33:08 -0700

    fix broken stylesheet link on Songbook App's login & detailed song list pages (also, switch to use config's "StaticPrefix")

--- ugsphp/views/login.php
+++ ugsphp/views/login.php
@@ -10,8 +10,8 @@ $errCssstyle = (strlen($model->ErrorMessage) > 0) ? 'block' : 'none';
 	<meta charset="utf-8" />
 	<title><?php echo($model->PageTitle); ?></title>
 	<meta name="generator" content="<?php echo($model->PoweredBy) ?>" />
-	<link rel="stylesheet" href="/css/editor/ugsEditorPlus.css" />
-	<link rel="stylesheet" href="/css/ugsphp.css" />
+	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsEditorPlus.merged.css" title="ugsEditorCss" />
+	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsphp.css" />
 </head>
 <body class="loginPage">
 <section class="contentWrap">
--- ugsphp/views/song-list-detailed.php
+++ ugsphp/views/song-list-detailed.php
@@ -41,7 +41,7 @@ function MakeRowHtml($song){
 	<meta charset="utf-8" />
 	<title><?php echo($model->PageTitle); ?></title>
 	<meta name="generator" content="<?php echo($model->PoweredBy) ?>" />
-	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/editor/ugsEditorPlus.css" />
+	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsEditorPlus.merged.css" title="ugsEditorCss" />
 	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsphp.css" />
 	<link rel="stylesheet" href="<?php echo($model->StaticsPrefix); ?>css/ugsEditorPlus.typeahead.css" />
 </head>
