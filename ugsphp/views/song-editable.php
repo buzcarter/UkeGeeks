88f71710786e9f430d744178908ecbb2a0a2f31f
BloodyBowlers <53180097+bloodybowlers@users.noreply.github.com>
2019-08-05 07:06:05 +0000

    ukeChordsCanvasWrapper position fix (#33)
    It was missing some basics classes.
    Thanks to @Louis-Coding for pointing that out !

--- ugsphp/views/song-editable.php
+++ ugsphp/views/song-editable.php
@@ -31,7 +31,7 @@ $editDlgCssClassName = $model->IsUpdateAllowed ? '' : 'isHidden';
 	</header>
 	<div class="metaInfo" id="songMeta"> </div>
 	<article id="ukeSongContainer" class="ugsLayoutTwoColumn ugs-song-wrap">
-    <div id="ukeChordsCanvasWrapper">
+    <div id="ukeChordsCanvasWrapper" class="ugs-diagrams-wrap ugs-grouped">
       <aside id="ukeChordsCanvas" class="ugs-diagrams-wrap ugs-grouped"></aside>
     </div>
 		<article id="ukeSongText" class="ugs-source-wrap">
